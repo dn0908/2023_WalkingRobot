87645b872e06c9180299ddd21b1ba9957efa778f
dn0908 <dahyun.diana.nam@gmail.com>
2023-10-26 00:21:06 +0900

    Added test codes for socket communication

---
+++ socket_pc.py
@@ -0,0 +1,43 @@
+see__author__ = 'dahyun'
+
+import threading
+import SocketServer
+import cv2
+import numpy as np
+import socket
+
+img=cv2.imread("frame.jpg",cv2.IMREAD_COLOR)
+cv2.imshow("TEST IMAGE",img)
+
+class KeyboardControl(SocketServer.BaseRequestHandler):
+    data=" "
+    global img
+    def handle(self):
+        try:
+            while self.data:
+                key = cv2.waitKey(1) & 0xFF
+                if key==255:
+                    key="No Keyboard Input"
+                elif key==ord('s'):
+                    key="STOP"
+                else:
+                    key=chr(key)
+                self.data = self.request.recv(1024)
+                self.request.sendall(key)
+                keyboard_data = self.data.decode()
+                print(keyboard_data)
+            cv2.destroyAllWindows()
+        finally:
+            print "Connection closed on thread 1"
+
+class ThreadServer(object):
+    def server_thread(host, port):
+        server = SocketServer.TCPServer((host, port), KeyboardControl)
+        server.serve_forever()
+    
+    ip=socket.gethostbyname(socket.getfqdn())
+    keyboard_thread = threading.Thread(target=server_thread(ip, 9999))
+    keyboard_thread.start()
+
+if __name__ == '__main__':
+    ThreadServer()
---
+++ socket_pi.py
@@ -0,0 +1,26 @@
+import time
+import cv2
+import numpy as np
+from socket import *
+import argparse
+
+# create a socket and bind socket to the host
+parser = argparse.ArgumentParser(description='Press IP adress and Port number')
+parser.add_argument('-ip', type=str ,default = '165.132.138.161')
+parser.add_argument('-port', type=int, default = 9999)
+
+a = parser.parse_args()
+ip = a.ip
+port = a.port
+
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect((ip, port))
+
+try:
+	while True:
+		client_socket.send("Fine Connection")		
+		data = client_socket.recv(1024)
+		print(data)
+		time.sleep(0.05)
+finally:
+	client_socket.close()
