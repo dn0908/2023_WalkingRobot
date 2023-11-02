4e2554fb16c67b52bda183b576566e5da0ce207f
dn0908 <dahyun.diana.nam@gmail.com>
2023-11-03 00:00:47 +0900

    added hc sensor codes

---
+++ HC_Class.py
@@ -0,0 +1,56 @@
+import RPi.GPIO as GPIO
+import time
+
+class HC_SR04:
+    def __init__(self, GPIO_ECHO):
+        # GPIO_TRIGGER = 11  # GPIO 17
+        # GPIO_ECHO = 22  # GPIO 25
+
+        self.GPIO_TRIGGER = 11
+        self.GPIO_ECHO = 22
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.GPIO_TRIGGER, GPIO.OUT)
+        GPIO.setup(self.GPIO_ECHO, GPIO.IN)
+
+    def hc_get_distance(self):
+        GPIO.output(self.GPIO_TRIGGER, False)
+
+        GPIO.output(self.GPIO_TRIGGER, True)
+        time.sleep(0.00001)
+        GPIO.output(self.GPIO_TRIGGER, False)
+
+        pulse_start = time.time()
+        pulse_end = time.time()
+
+        while GPIO.input(self.GPIO_ECHO) == 0:
+            pulse_start = time.time()
+
+        while GPIO.input(self.GPIO_ECHO) == 1:
+            pulse_end = time.time()
+
+        # Calculate pulse duration
+        pulse_duration = pulse_end - pulse_start
+
+        # Calculate dist 34300 cm/s
+        distance = (pulse_duration * 34300) / 2
+
+        return distance
+
+    def cleanup(self):
+        GPIO.cleanup()
+
+
+if __name__ == '__main__':
+    GPIO_TRIGGER = 11  # GPIO 17
+    GPIO_ECHO = 22  # GPIO 25
+
+    try:
+        sensor = HC_SR04()
+        while True:
+            distance = sensor.hc_get_distance()
+            print("거리: {:.2f} cm".format(distance))
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+        sensor.cleanup()
